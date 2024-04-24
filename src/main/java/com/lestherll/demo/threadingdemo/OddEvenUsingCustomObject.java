6622fbf0b08e56b00a8eecbf9386f96f474517dd
Lesther Jr Llacuna <ljllacuna5@gmail.com>
2024-04-24 18:55:25 +0100

    Update OddEvenUsingCustomObject

--- src/main/java/com/lestherll/demo/threadingdemo/OddEvenUsingCustomObject.java
+++ src/main/java/com/lestherll/demo/threadingdemo/OddEvenUsingCustomObject.java
@@ -1,60 +1,106 @@
 package com.lestherll.demo.threadingdemo;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class OddEvenUsingCustomObject {
-    private static final AtomicInteger current = new AtomicInteger(0);
+/*
+Custom helper range class
+ */
+class Range {
     private final int start;
     private final int end;
+    private final int step;
 
-    OddEvenUsingCustomObject(int start, int end) {
+    private int current;
+
+    Range(int start, int end, int step) {
         this.start = start;
         this.end = end;
-//        this.current = 0;
+        this.step = step;
+        this.current = start;
     }
+
+    Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.step = 1;
+        this.current = start;
+    }
+
+    private void setCurrent(int newCurrent) {
+        this.current = Math.min(newCurrent, this.end);
+    }
+
+    public void step() {
+        this.setCurrent(this.current + this.step);
+    }
+
+    public void step(int nStep) {
+        this.setCurrent(this.current + nStep);
+    }
+
+    public boolean hasEnded() {
+        return this.current >= this.end;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getStep() {
+        return step;
+    }
+}
+
+public class OddEvenUsingCustomObject {
+    private static final Range sharedObject = new Range(0, 10,1);
 
     public static void main(String[] args) {
-        OddEvenUsingCustomObject shared = new OddEvenUsingCustomObject(0, 10);
+        Thread oddThread1 = new Thread(OddEvenUsingCustomObject::printOddNumber);
+//        Thread oddThread2 = new Thread(OddEvenUsingCustomObject::printOddNumber);
+        Thread evenThread1 = new Thread(OddEvenUsingCustomObject::printEvenNumber);
 
-        Thread oddThread = new Thread(shared::printOddNumber);
-        Thread evenThread = new Thread(shared::printEvenNumber);
-
-        evenThread.start();
-        oddThread.start();
-
+        evenThread1.start();
+        oddThread1.start();
+//        oddThread2.start();
     }
 
-    public void printOddNumber() {
-        synchronized (current) {
-            while (current.get() <= 10) {
-                while (current.get() % 2 == 0) {
+    public static void printOddNumber() {
+        synchronized (sharedObject) {
+            while (!sharedObject.hasEnded()) {
+                while (sharedObject.getCurrent() % 2 == 0) {
                     try {
-                        current.wait();
+                        sharedObject.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                System.out.println(Thread.currentThread().getName() + " " + current + " ");
-                current.set(current.get() + 1);
-                current.notify();
+                System.out.println(Thread.currentThread().getName() + " " + sharedObject.getCurrent());
+                sharedObject.step();
+                sharedObject.notify();
             }
         }
     }
 
-    public void printEvenNumber() {
-        synchronized (current) {
-            while (current.get() <= 10) {
-                while (current.get() % 2 == 1) {
+    public static void printEvenNumber() {
+        synchronized (sharedObject) {
+            while (!sharedObject.hasEnded()) {
+                while (sharedObject.getCurrent() % 2 != 0) {
                     try {
-                        current.wait();
+                        sharedObject.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-                System.out.println(Thread.currentThread().getName() + " " + current + " ");
-//                current += 1;
-                current.set(current.get() + 1);
-                current.notify();
+                System.out.println(Thread.currentThread().getName() + " " + sharedObject.getCurrent());
+                sharedObject.step();
+                sharedObject.notify();
             }
         }
     }
